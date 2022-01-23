42a0ce533dbe32ff2e7e40631c496138ceeaba66
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2022-01-24 00:12:44 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { fetchCountries } from './fetchCountries';
+import renderCountriesTpl from '../src/templates/renderCountriesTpl';
+import renderCountryTpl from '../src/templates/renderCountryTpl';
+
 import './css/styles.css';
 
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
 const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener('input', debounce(onInputEnterValue, DEBOUNCE_DELAY));
+
+function onInputEnterValue(e) {
+  const searchQuery = e.target.value.trim();
+
+  if (!searchQuery) {
+    return cleaningRenderCountrys();
+  }
+
+  fetchCountries(searchQuery)
+    .then(countrys => {
+      if (countrys.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+        return cleaningRenderCountrys();
+      }
+      if (countrys.length >= 2 && countrys.length <= 10) {
+        refs.countryInfo.innerHTML = '';
+        Notify.success('You found some countries.');
+        cleaningRenderCountrys();
+        return renderCountrys(countrys);
+      }
+      if (countrys.length === 1) {
+        refs.countryList.innerHTML = '';
+        Notify.success('You found one country.');
+        cleaningRenderCountrys();
+        return renderCountry(countrys);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      errorHandler();
+      cleaningRenderCountrys();
+    });
+}
+
+function renderCountrys(countrys) {
+  const markup = renderCountriesTpl(countrys);
+  refs.countryList.insertAdjacentHTML('beforeend', markup);
+}
+
+function renderCountry(countrys) {
+  const markup = renderCountryTpl(countrys);
+  refs.countryInfo.insertAdjacentHTML('beforeend', markup);
+}
+
+function errorHandler() {
+  Notify.failure('Oops, there is no country with that name.');
+}
+
+function cleaningRenderCountrys() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+}
